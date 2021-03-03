7de66a025616919e3e586c5f3b2042a38adb7ee0
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2021-03-02 17:30:35 -0800

    tests: model: Support message_id variation in reaction_event_factory.

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -1343,7 +1343,7 @@ class TestModel:
 
     @pytest.fixture
     def reaction_event_factory(self):
-        def _factory(*, op: str):
+        def _factory(*, op: str, message_id: int):
             return {
               'emoji_code': '1f44d',
               'id': 2,
@@ -1353,7 +1353,7 @@ class TestModel:
                   'full_name': 'Foo Boo'
               },
               'reaction_type': 'unicode_emoji',
-              'message_id': 1,
+              'message_id': message_id,
               'emoji_name': 'thumbs_up',
               'type': 'reaction',
               'op': op,
@@ -1402,13 +1402,12 @@ class TestModel:
         reaction_event_factory, reaction_event_index_factory,
         op,
     ):
-        reaction_event = reaction_event_factory(op=op)
+        reaction_event = reaction_event_factory(op=op, message_id=99)
         model.index = reaction_event_index_factory(
             [
                 (1, None),
             ]
         )
-        model._update_rendered_view = mocker.Mock()
         previous_index = deepcopy(model.index)
 
         model._handle_reaction_event(reaction_event)
@@ -1421,7 +1420,7 @@ class TestModel:
         self, mocker, model,
         reaction_event_factory, reaction_event_index_factory,
     ):
-        reaction_event = reaction_event_factory(op="add")
+        reaction_event = reaction_event_factory(op="add", message_id=1)
         model.index = reaction_event_index_factory(
             [
                 (1, [(1, "unicode_emoji", "1232", "thumbs_up")]),
@@ -1441,7 +1440,7 @@ class TestModel:
         self, mocker, model,
         reaction_event_factory, reaction_event_index_factory,
     ):
-        reaction_event = reaction_event_factory(op="remove")
+        reaction_event = reaction_event_factory(op="remove", message_id=1)
         model.index = reaction_event_index_factory(
             [
                 (1, [(1, "unicode_emoji", "1232", "thumbs_up")]),
