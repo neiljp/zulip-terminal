0d1a0638cb91c20bdf986dcb2ebb2321bdf58f9a
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2021-05-21 14:21:24 -0700

    refactor: core/ui/boxes/run: Simplify implicitly joined strings.
    With the longer maximum line-length, this allows simplification of
    strings spanning multiple lines which are implicitly combined. Applying
    `black` will soon tidy this further, but does not merge adjacent
    implicitly joinable strings; this commit handles this manually to avoid
    such strings being placed manually on the same lines, or split
    awkwardly.

--- zulipterminal/cli/run.py
+++ zulipterminal/cli/run.py
@@ -166,19 +166,15 @@ def get_api_key(realm_url: str) -> Tuple[requests.Response, str]:
 
 
 def fetch_zuliprc(zuliprc_path: str) -> None:
-    print(in_color('red', f"zuliprc file was not found at {zuliprc_path}")
-          + "\nPlease enter your credentials to login into your"
-            " Zulip organization."
-            "\n"
-            "\nNOTE: The " + in_color('blue', "Zulip URL")
-          + " is where you would go in a web browser to log in to Zulip."
-            "\nIt often looks like one of the following:"
-            "\n   " + in_color('green', "your-org.zulipchat.com")
-          + " (Zulip cloud)"
-            "\n   " + in_color('green', "zulip.your-org.com")
-          + " (self-hosted servers)"
-            "\n   " + in_color('green', "chat.zulip.org")
-          + " (the Zulip community server)")
+    print(f"{in_color('red', f'zuliprc file was not found at {zuliprc_path}')}"
+          f"\nPlease enter your credentials to login into your Zulip organization."
+          f"\n"
+          f"\nNOTE: The {in_color('blue', 'Zulip URL')}"
+          f" is where you would go in a web browser to log in to Zulip."
+          f"\nIt often looks like one of the following:"
+          f"\n   {in_color('green', 'your-org.zulipchat.com')} (Zulip cloud)"
+          f"\n   {in_color('green', 'zulip.your-org.com')} (self-hosted servers)"
+          f"\n   {in_color('green', 'chat.zulip.org')} (the Zulip community server)")
     realm_url = styled_input('Zulip URL: ')
     if realm_url.startswith("localhost"):
         realm_url = f"http://{realm_url}"
@@ -397,11 +393,11 @@ def main(options: Optional[List[str]]=None) -> None:
         print("   theme '{}' specified {}.".format(*theme_to_use))
         complete, incomplete = complete_and_incomplete_themes()
         if theme_to_use[0] in incomplete:
-            print(in_color('yellow',
-                           "   WARNING: Incomplete theme; "
-                           "results may vary!\n"
-                           "      (you could try: {})".
-                           format(", ".join(complete))))
+            incomplete_theme_warning = (
+                "   WARNING: Incomplete theme; results may vary!\n"
+                "      (you could try: {})".format(", ".join(complete))
+            )
+            print(in_color('yellow', incomplete_theme_warning))
         print("   autohide setting '{}' specified {}."
               .format(*zterm['autohide']))
         if zterm['footlinks'][1] == ZULIPRC_CONFIG:
