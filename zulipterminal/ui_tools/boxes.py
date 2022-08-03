40f9c168962d01c37b58c0300a5ea1343a36c5c9
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2022-08-02 20:08:32 -0700

    fixup(neiljp): tidy code

--- zulipterminal/ui_tools/boxes.py
+++ zulipterminal/ui_tools/boxes.py
@@ -640,23 +640,25 @@ class WriteBox(urwid.Pile):
             stream_data, text[prefix_length:], self.view.pinned_streams
         )
 
-        muted_stream_names = [
+        muted_streams = [
+            self.model.stream_dict[stream_id]["name"]
+            for stream_id in self.model.muted_streams
+        ]
+        matching_muted_streams = [
             stream_name
             for stream_name in matched_streams
-            if stream_name
-            in [
-                self.model.stream_dict[stream_id]["name"]
-                for stream_id in self.model.muted_streams
-            ]
+            if stream_name in muted_streams
         ]
         pinned_streams = [stream["name"] for stream in self.view.pinned_streams]
         pinned_unpinned_separator = len(set(pinned_streams) & set(matched_streams))
-        for muted_stream in muted_stream_names:
-            matched_streams.remove(muted_stream)
-            if muted_stream in pinned_streams:
-                matched_streams.insert(pinned_unpinned_separator - 1, muted_stream)
+        for matching_muted_stream in matching_muted_streams:
+            matched_streams.remove(matching_muted_stream)
+            if matching_muted_stream in pinned_streams:
+                matched_streams.insert(
+                    pinned_unpinned_separator - 1, matching_muted_stream
+                )
             else:
-                matched_streams.append(muted_stream)
+                matched_streams.append(matching_muted_stream)
 
         matched_stream_typeaheads = format_string(matched_streams, "#**{}**")
         return matched_stream_typeaheads, matched_streams
