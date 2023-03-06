e11682d16115de755c34b0f871251b718213d308
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2023-03-06 13:25:42 -0800

    zuliprc: Add minimal security_issues_present() method.
    Test added.

--- tests/zuliprc/test_zuliprc.py
+++ tests/zuliprc/test_zuliprc.py
@@ -10,3 +10,10 @@ def test_init__with_path(zuliprc_factory: ZuliprcFactoryT) -> None:
 def test_init__with_string(zuliprc_factory: ZuliprcFactoryT) -> None:
     zuliprc_path = zuliprc_factory(api=None, config=None)
     ZuliprcFile(str(zuliprc_path))
+
+
+def test_security_issues_present__return_None(zuliprc_factory: ZuliprcFactoryT) -> None:
+    zuliprc_path = zuliprc_factory(api=None, config=None)
+    zuliprc_file = ZuliprcFile(zuliprc_path)
+    result = zuliprc_file.security_issues_present()  # type: ignore[func-returns-value]
+    assert result is None
--- zulipterminal/zuliprc.py
+++ zulipterminal/zuliprc.py
@@ -9,3 +9,7 @@ class ZuliprcFile:
     def __init__(self, zuliprc_path: Union[str, Path]) -> None:
         """Initialize with the path to the zuliprc file to work with."""
         self._zuliprc_path: Path = Path(zuliprc_path)
+
+    def security_issues_present(self) -> None:
+        """Determine if any security issues are present."""
+        return None
